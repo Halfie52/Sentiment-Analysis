e1ba8f56d706339fedfb42a1add6576feabca0ef
Halfie52 <52163949+Halfie52@users.noreply.github.com>
2019-09-14 13:07:25 +0100

    Update #2.py

--- #2.py
+++ #2.py
@@ -13,7 +13,7 @@ access_token_secret = ''
 
 #authenticate with twitter , basically login with code
 
-auth = tweepy.OAuthhandler(consumer_key, consumer_secret)
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
